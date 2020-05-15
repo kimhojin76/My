d69cd74db9c1bbf8aa6f435d696a0a2a16fcd22f
crazybear1 <painmasters@naver.com>
2020-05-15 12:14:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication7/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final EditText nametext = (EditText) findViewById(R.id.id_text1);
+        final EditText passwordtext = (EditText) findViewById(R.id.login_password1);
+        Button loginbutton = (Button) findViewById(R.id.login_button1);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nametext.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), SingUp_Activity.class);
+
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication7/SingUp_Activity.java
@@ -0,0 +1,4 @@
+package com.example.myapplication7;
+
+public class SingUp_Activity {
+}
